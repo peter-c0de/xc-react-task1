bc2ec26b18ca49c9394bb6ef2d507cf6256bcd13
Peter CODE <pet3r.code@gmail.com>
2025-11-17 12:05:10 +0200

    Added: src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [position, setPosition] = useState(0);
+
+  const moveRight = () => {
+    setPosition((prev) => prev + 20);
+    console.log("New Position: ", position+20);
+
+  };
+
+  const moveLeft = () => {
+    
+    if ( position > 0 )  {
+      setPosition((prev) => prev - 20);
+      console.log("New Position: ", position-20);
+    }
+
+  };
+
+  return (
+    <div className="container">
+      <div
+        className="box"
+        style={{ left: position + "px" }}
+      ></div>
+
+      <div className="buttons">
+        <button onClick={moveLeft}>Move Left</button>
+        <button onClick={moveRight}>Move Right</button>
+      </div>
+      { console.log("Current Position: ", position) }
+    </div>
+  );
+}
+
+export default App;
